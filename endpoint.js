45302566e55a1a8e612b498a08f31ab4b19df6f8
RakhmatilloRR <rrr.engineer.94@gmail.com>
2022-06-13 09:13:13 +0500

    first message

---
+++ endpoint.js
@@ -0,0 +1,23 @@
+// const id = '62a6b2330d09d870f496dce1';
+// post: url = http://localhost:5000/api/customers
+// update: url = http://localhost:5000/api/customers/id
+// readAll: url = http://localhost:5000/api/customers
+// readone: url = http://localhost:5000/api/customers/id
+// delete: url = http://localhost:5000/api/customers/id
+
+// github: https://github.com/RakhmatulloRR/MERN-CRUD
+
+// register: 
+// const body:
+{ 
+  "name": "name2", 
+  "email": "email2@gmail.com", 
+  "password": "Password2&123", 
+  "isAdmin": false 
+  }
+// login:
+// body:
+  {
+    "email": "admin@gmail.com",
+    "password": "admin&123"
+}
